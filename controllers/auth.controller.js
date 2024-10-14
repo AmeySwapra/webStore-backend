9973995e0d0bdf08a640357ba1e2b0846ced8454
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-10-14 17:43:42 +0530

    securing the jwt cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -68,7 +68,7 @@ export  const login = async(req, res) => {
 
         res.cookie('token', token, {
             httpOnly : true,
-            // secure: true, make true in prodution
+             secure: true, 
             maxAge: age
         }).status(200).json(userInfo)
 
